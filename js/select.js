a42dde69629607cfd7dde22a0d03302b4dea622b
Pobporn <pobpornbu@gmail.com>
2015-05-15 17:14:21 -0700

    [Progress] dropdown

---
+++ js/select.js
@@ -0,0 +1,56 @@
+$(function() {
+
+// Dropdown
+$('select').each(function(){
+    var $this = $(this), numberOfOptions = $(this).children('option').length;
+  
+    $this.addClass('select-hidden'); 
+    $this.wrap('<div class="select__wrap"></div>');
+    $this.after('<div class="select__selected"></div>');
+
+    var $styledSelect = $this.next('div.select__selected');
+    $styledSelect.text($this.children('option').eq(0).text());
+  
+    var $list = $('<ul />', {
+        'class': 'select__options'
+    }).insertAfter($styledSelect);
+  
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+  
+    var $listItems = $list.children('li').addClass('select__option');
+
+	$styledSelect.on("click", function(e){
+        e.stopPropagation();
+        if($styledSelect.hasClass('active')){
+            $styledSelect.removeClass('active');
+            $list.hide();
+        }else{
+            $('.select__selected.active').each(function(){
+                $(this).removeClass('active').next('ul.select__options').hide();
+            });
+            $(this).toggleClass('active').next('ul.select__options').toggle();
+        }
+	});
+
+    $listItems.click(function(e) {
+        e.stopPropagation();
+        // $this.attr("href", $(this))
+        $styledSelect.text($(this).text()).removeClass('active');
+        // $this.val($(this).attr('rel'));
+        // $list.hide();
+        console.log($(this).val());
+    });
+  
+    $(document).click(function() {
+        $styledSelect.removeClass('active');
+        $list.hide();
+    });
+
+});
+
+});
