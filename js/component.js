54d4998b3681d3506323a4a97f33debbe49a7823
Pobporn <pobpornbu@gmail.com>
2015-06-15 09:51:16 -0700

    Tab

--- js/component.js
+++ js/component.js
@@ -1,9 +1,19 @@
-$(function() {
+(function($) {
 
-// Textarea
-autosize($('textarea'));
-$('.js-comment-textarea').on('focus', function() {
-	$(this).parent().toggleClass('active');
-});
+    // Textarea
+    autosize($('textarea'));
+    $('.js-comment-textarea').on('focus', function() {
+    	$(this).parent().toggleClass('active');
+    });
 
-});
+    // Tab
+    $('#myTab').on('click', 'a', function(e) {
+      e.preventDefault();
+      $(this).tab('show');
+    //   $(this).toggleClass('focus');
+    //   $(this).css('background', 'red');
+    // });//.on('click', 'li', function(){
+        //$(this).removeClass('active');
+    });
+    
+})(jQuery);
