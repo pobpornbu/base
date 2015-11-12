e38c480664b29a91507a2128e305837c87f8337c
Pobporn <pobpornbu@gmail.com>
2015-11-12 15:43:16 +0700

    simple side menu

--- js/component.js
+++ js/component.js
@@ -7,14 +7,14 @@ $(function() {
     });
 
     // Tab
-    $('#myTab').on('click', 'a', function(e) {
-      e.preventDefault();
-      $(this).tab('show');
+    // $('#myTab').on('click', 'a', function(e) {
+    //   e.preventDefault();
+    //   $(this).tab('show');
     //   $(this).toggleClass('focus');
     //   $(this).css('background', 'red');
     // });//.on('click', 'li', function(){
         //$(this).removeClass('active');
-    });
+    // });
 
     // Adjust Font Size
     $(".js-btn-adjustfont").textresizer({
@@ -37,32 +37,68 @@ $(function() {
         //  //bind social share button
         //  Apps.social.bindShareButton($dialog,$button);
         // },
-        functionReady: function(){
-          $(this).addClass('active');
-        },
-        functionAfter: function(){
-          $(this).removeClass('active');
-        },
-        minWidth: 0
+            functionReady: function(){
+              $(this).addClass('active');
+            },
+            functionAfter: function(){
+              $(this).removeClass('active');
+            }
+                
         }).click(function(e){e.preventDefault();});
-    // }
-  });
+        // }
+    });
 
     // Example Malihu Scroll
-    $(".js-nav-scroll-news, .js-nav-scroll-program").mCustomScrollbar({
-        setHeight: 300,
-        theme: 'minimal-dark',
-        mouseWheel:{ enable: true, preventDefault: true}
-    });
+    // $(".js-nav-scroll-news, .js-nav-scroll-program").mCustomScrollbar({
+    //     setHeight: 300,
+    //     theme: 'minimal-dark',
+    //     mouseWheel:{ enable: true, preventDefault: true}
+    // });
 
-    $(".js-nav-scroll-gallery").mCustomScrollbar({
-        setHeight: 300,
-        theme: 'minimal-dark',
-        mouseWheel:{ enable: true, preventDefault: true, scrollAmount: 600, deltaFactor: 600}
+    // $(".js-nav-scroll-gallery").mCustomScrollbar({
+    //     setHeight: 300,
+    //     theme: 'minimal-dark',
+    //     mouseWheel:{ enable: true, preventDefault: true, scrollAmount: 600, deltaFactor: 600}
+    // });
+    $('.menu__icon').click(function(){
+        $(this).toggleClass('open');
     });
-
 });
 
+    $('.js-tab-list').each(function(){
+        var $this           = $(this);
+        var $currentwrap    = $this.find('div.active');
+        var $currentlink    = $currentwrap.find('a');
+        var $targetpanel    = $($currentlink.data('target'));
+
+        //For Desktop
+        $this.on('mouseover', '.js-tab-control', function(e) {
+            e.preventDefault();
+            var $currentlink = $(this);
+            var id = $currentlink.data('target');
+            if (id && !$currentlink.is('.active')) {
+                $targetpanel.removeClass('active');
+                $currentwrap.removeClass('active');
+
+                $targetpanel = $(id).addClass('active');
+                $currentwrap = $currentlink.parent().addClass('active');
+            }
+        });
+
+        //For mobile
+        /*if(Modernizr.touch){
+            $this.on('click', function(e){
+                e.preventDefault();
+                e.stopPropagation();
+            });
+            $this.on('click', '.js-tab-control', function(){
+                var getLink             = $(this).data('href'); //get attr data-href for open link
+                if(getLink){
+                    window.location.href = getLink;
+                }       
+            });
+        }*/
+    });
 
 
 function printImg() {
--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
-  $('.menu__icon').click(function(){
-    $(this).toggleClass('open');
+  // Tab
+  $('#myTab').on('click', 'a', function(e) {
+    e.preventDefault();
+    $(this).tab('show');
   });
+
+  $('.js-menu-side').each(function(){
+      var $this  = $(this),
+      		$btn   = $this.find('.js-menu-btn'),
+          $list  = $this.find('.js-menu-list'),
+          $overlay = $this.find('.js-overlay');
+
+      $this.on('click', '.js-menu-btn', function(e){
+          e.preventDefault();
+					// if($('.cd-primary-nav').hasClass('nav-is-visible')) {
+          openMenu();
+          // }
+      });
+
+			//close lateral menu on mobile 
+			$('.js-overlay').on('click swipeleft swiperight', function(){
+				if($('.js-menu-side').hasClass('is-menu-open')) {
+					closeMenu();
+					// toggleSearch('close');
+					$('.js-overlay').removeClass('open');
+				}
+			});
+
+			$list.on('click', function(){
+				if($(this).hasClass('open')) {
+					closeMenu();
+				}
+			});
+
+      function openMenu() {
+        $btn.toggleClass('open');
+        $list.toggleClass('open');
+        $overlay.toggleClass('open');
+        $this.toggleClass('is-menu-open');
+      }
+
+      function closeMenu() {
+        $btn.removeClass('open');
+        $list.removeClass('open');
+        $overlay.removeClass('open');
+        $this.removeClass('is-menu-open');
+      }
+  });
+
 });
