25cbf9a46634ee1db64e7cb504cf84d65771e8a5
CSS Maker <pobpornbu@gmail.com>
2016-02-14 02:04:33 +0700

    finish basic function for dropdown plugin

--- js/jquery.selectx.js
+++ js/jquery.selectx.js
@@ -34,33 +34,28 @@
 
         // this.$el.data(name, this);
 
-        // this.$header = this.$elem.find('.header');
-        // this.$body   = this.$el.find('.body');
-
         // a unique namespace per instance, for easy selective unbinding
         // safeguard our plugin from breaking in the event that another script
         this.namespace = 'dropdown-'+ Math.round(Math.random()*100000);
 
         this.$elem.hide();
         this.$elem.wrap('<div class="select__wrap"></div>');
+        this.$select = this.$elem.parent('.select__wrap');
 
-        var numberOfOptions = this.$elem.children('option').length,
-            $selectWrap = this.$elem.parent();
-        
         if( this.options.theme ){
-            $selectWrap.addClass( this.options.theme );
+            this.$select.addClass( this.options.theme );
         }
 
         this.$elem.after('<div class="select__selected"></div>');
+        this.$selected = this.$elem.next('.select__selected');
 
-        var $styledSelect = this.$elem.next('div.select__selected'),
-            $list = $('<ul />', {'class': 'select__options'}).insertAfter($styledSelect);
-            // $listItems = $list.children('li');
+        var $list = $('<ul />', {'class': 'select__options'}).insertAfter(this.$selected),
+            numOpt = this.$elem.children('option').length;
 
-        $styledSelect.text(this.$elem.children('option').eq(0).text());
+        this.$selected.text(this.$elem.children('option').eq(0).text());
 
 
-        for (var i = 0; i < numberOfOptions; i++) {
+        for (var i = 0; i < numOpt; i++) {
             $('<li />', {
                 text: this.$elem.children('option').eq(i).text(),
                 'data-value': this.$elem.children('option').eq(i).val(),
@@ -69,10 +64,8 @@
         }
 
         $list.children('li').first().hide();
-        
+
         // place in right position : after created
-        this.$select = this.$elem.parent('.select__wrap');
-        this.$selected = this.$elem.next('.select__selected');
         this.$lists = this.$elem.siblings('.select__options');
         this.$list = this.$lists.children();
         this.init();
@@ -96,67 +89,34 @@
 
         this.$selected.on("click." + self._name, function(event){
             event.stopPropagation();
-            self.toggleSelect();
-            
-            // $('document').one('click', function(event) {
-                // if(!$(event.target).closest('.select__wrap').length){
-                // if(!$(event.target).is(this.$select)){
-                // if (this.$selected.hasClass("active") && !this.$select.is(e.target) && !this.$select.has(e.target).length){
-                //     if(this.$selected.hasClass("active")) {
-                // if ($(event.target).parents('.select__wrap').length==0) {
-                //         console.log('You clicked outside.');
-                //         self.closeSelect();
-                //         // $(this).off(function(){
-                //         //     self.openSelect();
-                //         // });
-                // }
-                //     }
-                // }
-                // event.stopPropagation();
-            // });
+            self.openSelect();
         });
-        
+
         this.$list.on('click.' + self._name, function(event){
             event.stopPropagation();
             $(this).each(function(index, element){
                 self.replaceSelected(index, element);
-                self.toggleSelect();
+                self.closeSelect();
             });
         });
-        // $('document').one('clickoutside', function(callback) {
-        //     console.log('this');
-        //     self.clickOutside(callback);
-        // });    
-        // var q = function() { alert('clicked outside of the element') }; 
 
-        // this.$select.clickoutside(q);
-        // this.$select.on("clickoutside", function() { alert('clicked outside of the element')});
-        this.$select.on('click.'+self._name, function(){
-            self.clickoutside();
+        $(document).on('click.' + self._name, function(e){
+            e.stopPropagation();
+            self.closeSelect();
         });
-    };
-
-    Plugin.prototype.toggleSelect = function() {
-        this.$selected.toggleClass('active').next('.select__options').toggle();
     };
 
     Plugin.prototype.openSelect = function() {
-        // if(this.$selected.hasClass('active')){
-        //     console.log('close');
-        //     this.$selected.removeClass('active').next('ul.select__options').hide();
-        // }else{
-        //     console.log('toggle');
-        //     $('.select__selected.active').each(function(){
-        //         $(this).removeClass('active').next('ul.select__options').hide();
-        //     });
-        //     this.$selected.toggleClass('active').next('.select__options').toggle();
-            this.$selected.addClass('active').next('.select__options').show();
-        // }
+        $('.select__selected.active').each(function(){
+            console.log('is_open');
+            $(this).removeClass('active').next('.select__options').hide();
+        });
+        this.$selected.toggleClass('active').next('.select__options').toggle();
     };
 
     Plugin.prototype.replaceSelected = function(index, element) {
-        var self = this, 
-            $element = $(element), 
+        var self = this,
+            $element = $(element),
             opted = $element.text(),
             link = $element.data('value').toLowerCase();
 
@@ -167,50 +127,21 @@
             window.location.href= link;
 
         }else if(self.options.item == 'external-link'){
-            
+
             window.open( link, '_blank');
 
         }else if(self.options.item == 'text'){
-            
+
             self.$selected.text(opted);
-            // $this.val($element.attr('rel')).trigger('change');        
+            // $this.val($element.attr('rel')).trigger('change');
         }
     };  // ****** Cannot access text of clicked li
 
     Plugin.prototype.closeSelect = function() {
-        // if (this.$selected.hasClass("active") && !this.$select.is(e.target) && !this.$select.has(e.target).length){
-            // if(this.$selected.hasClass("active")) {
-                console.log('close');
-                this.$selected.removeClass('active').next('.select__options').hide();
-            // }
-        // }
+        console.log('close');
+        this.$selected.removeClass('active').next('.select__options').hide();
+        $(document).off('click.'+self._name)
     }; // ****** Cannot open again after closing
-
-    // Plugin.prototype.clickoutside = function(callback) {
-    //     var outside = 1, self = this;
-    //     self.cb = callback;
-    //     self.click(function() {
-    //         outside = 0;
-    //     });
-    //     $(document).click(function() {
-    //         outside && self.cb();
-    //         outside = 1;
-    //     });
-    //     return this;
-    // };
-
-    $.fn.clickoutside = function(callback) {
-        var outside = 1, self = $(this);
-        self.cb = callback;
-        this.click(function() {
-            outside = 0;
-        });
-        $(document).click(function() {
-            outside && self.cb();
-            outside = 1;
-        });
-        return $(this);
-    };
 
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
